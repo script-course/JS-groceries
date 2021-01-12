c8329c2b9545192e39b88162739f81a63ab10a03
Goosterhof <gerard@script.nl>
2021-01-12 09:56:34 +0100

    Update script.js with reduce function

--- Jasper/script.js
+++ Jasper/script.js
@@ -2,16 +2,16 @@ const GROCERIES = document.getElementById('groceries');
 const ARTICLES = Array.from(document.getElementsByClassName('article'))
 
 GROCERIES.onchange = () => {
-    let groceriesTotal = 0
-    ARTICLES.forEach(article => {
-        let count = article.getElementsByClassName('articleCount')[0].value
-        let price = article.getElementsByClassName('articlePrice')[0].innerHTML
-        let total = count * price
+    const total = ARTICLES.reduce((groceriesTotal, article) => {
+        const count = article.getElementsByClassName('articleCount')[0].value
+        const price = article.getElementsByClassName('articlePrice')[0].innerHTML
+        const total = count * price
 
         article.getElementsByClassName('articlePriceTotal')[0].innerHTML = makeInteger(total)
-        groceriesTotal += total;
-    });
-    document.getElementById('allArticlesPriceTotal').innerHTML =  makeInteger(groceriesTotal)
+        return groceriesTotal + total;
+    }, 0)
+    
+    document.getElementById('allArticlesPriceTotal').innerHTML =  makeInteger(total)
 }
 
 function makeInteger(value){
